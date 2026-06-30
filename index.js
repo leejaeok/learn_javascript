234c98d2c77b0925fb03496511408483d547d0b3
leejaeok <hoyaljo@gmail.com>
2026-06-30 10:37:18 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const userName = "옥";
+console.log("🚀 ~ userName:", userName); // 변수 선택 후 ctrl + alt + L
+console.log(userName);
+console.log("userName", userName);
+
+const userAge = 43;
+const userMBTI = "ISFJ";
+const isLoggedin = false;
+console.log("🚀 ~ isLoggedin:", isLoggedin);
+console.log("Type isLoggedin", typeof isLoggedin);
