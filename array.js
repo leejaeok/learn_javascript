100e0834e517c4f94d8a735f289ea7e1365317e5
leejaeok <hoyaljo@gmail.com>
2026-06-30 16:07:24 +0900

    배열과 객체 commit

---
+++ array.js
@@ -0,0 +1,8 @@
+const arrTest = [1, 2, 3];
+const arrTestObejct = [
+  { name: 1, value: "test1" },
+  { name: 2, value: "test2" },
+];
+
+console.log(arrTest[0]);
+console.log(arrTestObejct[0].value);
